b68f16f391daff825b13a0c24b5eeaf34442d956
Rebecca <rebecca90469@gmail.com>
2024-05-17 17:32:16 -0400

    Initial build for single photo component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,22 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
+const PhotoListItem = (props) => {
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
-};
+  return (
+    <main>
+      <img className="uploaded_image" src= {props.image} />
+      <section className= "user-info">
+        <img className="profile_image" src= {props.profilePic}/>
+        <br />
+        <span>{props.username}</span>
+        <br />
+        <span>{props.city}, {props.country}</span>
+      </section>
+    </main>
+  );
+}
+
 
 export default PhotoListItem;
