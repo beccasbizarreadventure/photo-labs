bd3867ba97815fe82e6f728d00c9678540ebfb9d
Rebecca <rebecca90469@gmail.com>
2024-05-18 20:34:35 -0400

    Dynamic rendering for PhotoList of PhotoListItems mapped array

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,13 @@
 import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const sampleArray = [sampleDataForPhotoListItem, sampleDataForPhotoListItem, sampleDataForPhotoListItem]
+import PhotoList from 'components/PhotoList';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {
-        sampleArray.map((photoItem, index) => (
-          <PhotoListItem photo={photoItem} key={index}/>
-        ))
-      }
+      <PhotoList />
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
 
-const PhotoListItem = (props) => {
+const PhotoListItem = ({photo}) => {
 
 
   return (
     <main className="photo-list__item">
       <PhotoFavButton/>
-      <img className="photo-list__image" src= {props.photo.imageSource} />
+      <img className="photo-list__image" src= {photo.urls.regular} />
       <section className= "photo-list__user-details">
-        <img className="photo-list__user-profile" src= {props.photo.profile}/>
+        <img className="photo-list__user-profile" src= {photo.user.profile}/>
       <section className="photo-list__user-info">
-        <span className="photo-list__user-details">{props.photo.username}</span>
-        <span className="photo-list__user-location">{props.photo.location.city}, {props.photo.location.country}</span>
+        <span className="photo-list__user-details">{photo.user.name}</span>
+        <span className="photo-list__user-location">{photo.location.city}, {photo.location.country}</span>
       </section>
       </section>
     </main>
