3485795eaa22dc3b4987ba9c58dfc228517502ba
Rebecca <rebecca90469@gmail.com>
2024-05-20 18:48:19 -0400

    Start of additions to render in modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 
-import '../styles/PhotoDetailsModal.scss'
+import PhotoList from 'components/PhotoList';
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = ({closeModal, selectedPhoto}) => {
+import '../styles/PhotoDetailsModal.scss';
+
+const PhotoDetailsModal = ({ closeModal, selectedPhoto }) => {
   console.log(selectedPhoto);
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={closeModal}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      <img className="photo-details-modal__image" src={selectedPhoto.urls.regular} />
+      <section className="photo-details-modal__header">
+        <img className="photo-details-modal__photographer-profile" src={selectedPhoto.user.profile} />
+        <section className="photo-details-modal__photographer-info">
+          <span>{selectedPhoto.user.name}</span>
+          <span className="photo-details-modal__photographer-location">{selectedPhoto.location.city}, {selectedPhoto.location.country}</span>
+        </section>
+      </section>
     </div>
   )
 };
+
 
 export default PhotoDetailsModal;
