1d77912ac4ec86acd3131d089cde0472f8ca28f1
Rebecca <rebecca90469@gmail.com>
2024-05-20 23:21:12 -0400

    Add related photos to modal. Reuse PhotoList by making some of the props passed down set to empty/optional

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
 
-const PhotoList = ({ photos, handleLike, isLiked, selectPhoto }) => {
+const PhotoList = ({ photos,
+  handleLike = () => { }, 
+  isLiked = [], 
+  selectPhoto = () => { } 
+}) => {
 
   return (
     <ul className="photo-list">
       {
         photos.map((photoItem, id) => (
-          <PhotoListItem photo={photoItem} key={id} handleLike={() => handleLike(photoItem.id)} isLiked={isLiked.includes(photoItem.id)} selectPhoto={selectPhoto} />
+          <PhotoListItem photo={photoItem} key={photoItem.id} handleLike={() => handleLike(photoItem.id)} isLiked={isLiked.includes(photoItem.id)} selectPhoto={selectPhoto} />
         ))
       }
     </ul>
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -6,27 +6,23 @@ import closeSymbol from '../assets/closeSymbol.svg';
 import '../styles/PhotoDetailsModal.scss';
 
 const PhotoDetailsModal = ({ closeModal, selectedPhoto }) => {
-
-  const relatedPhotos = selectedPhoto.similar_photos;
-  console.log(selectedPhoto);
-
+  const relatedPhotosArray = Object.values(selectedPhoto.similar_photos);
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={closeModal}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      <img className="photo-details-modal__image" src={selectedPhoto.urls.regular} />
+      <img className="photo-details-modal__image" src={selectedPhoto.urls.full} />
       <section className="photo-details-modal__header">
         <img className="photo-details-modal__photographer-profile" src={selectedPhoto.user.profile} />
         <section className="photo-details-modal__photographer-info">
-          <span>{selectedPhoto.user.name}</span>
+          <span className="photo-details-modal__photographer-details">{selectedPhoto.user.name}</span>
           <span className="photo-details-modal__photographer-location">{selectedPhoto.location.city}, {selectedPhoto.location.country}</span>
         </section>
       </section>
-      <div className="photo-details-modal__top-bar">
-        <section className='photo-details-modal__images'>
-          <PhotoList photos={relatedPhotos} />
-        </section>
+      <h3>Similar Photos</h3>
+      <div>
+        <PhotoList photos={relatedPhotosArray} />
       </div>
     </div>
   )
