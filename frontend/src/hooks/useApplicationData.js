9f6b045e41e116695590a1e37869d2c529739804
Rebecca <rebecca90469@gmail.com>
2024-05-29 13:28:06 -0400

    Add some logic

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -19,14 +19,14 @@ function reducer(state, action) {
 
   switch (type) {
     // LIKE/UNLIKE A PHOTO STATE MANAGEMENT 
-    case ACTIONS.ADD_FAV_PHOTO:
-      return { ...state, favouritePhotos: [...state.favouritePhotos, payload.photoId] };
+    // case ACTIONS.ADD_FAV_PHOTO:
+    //   return { ...state, favouritePhotos: [...state.favouritePhotos, payload.photoId] };
 
-    case ACTIONS.REMOVE_FAV_PHOTO:
-      return { ...state, favouritePhotos: [...state.favouritePhotos.filter((id) => id !== payload.photoId)] };
+    // case ACTIONS.REMOVE_FAV_PHOTO:
+    //   return { ...state, favouritePhotos: [...state.favouritePhotos.filter((id) => id !== payload.photoId)] };
 
-    case ACTIONS.DISPLAY_FAVOURITE_PHOTOS:
-      return { ...state, photoData: [...state.photoData.filter((photo) => state.favouritePhotos.includes(photo.id))] };
+    // case ACTIONS.DISPLAY_FAVOURITE_PHOTOS:
+    //   return { ...state, photoData: [...state.photoData.filter((photo) => state.favouritePhotos.includes(photo.id))] };
 
     // STATE MANAGEMENT FOR SELECTING ONE TOPIC OR ONE PHOTO  
     // state.photoData.find using the whole photo item found by ID allows this state to function in a reusable manner
@@ -67,6 +67,8 @@ const useApplicationData = () => {
     topicData: [],
   });
 
+
+
   // FETCH PHOTO DATA
   useEffect(() => {
     axios.get("http://localhost:8001/api/photos")
@@ -100,14 +102,14 @@ const useApplicationData = () => {
   }, [state.selectedTopic]);
 
   // LIKE/UNLIKE A PHOTO
-  const toggleFavourite = (photoId) => {
-    state.favouritePhotos.includes(photoId) ? dispatch({ type: ACTIONS.REMOVE_FAV_PHOTO, payload: { photoId } }) :
-      dispatch({ type: ACTIONS.ADD_FAV_PHOTO, payload: { photoId } });
-  };
+  // const toggleFavourite = (photoId) => {
+  //   state.favouritePhotos.includes(photoId) ? dispatch({ type: ACTIONS.REMOVE_FAV_PHOTO, payload: { photoId } }) :
+  //     dispatch({ type: ACTIONS.ADD_FAV_PHOTO, payload: { photoId } });
+  // };
 
-  const handleShowFavourites = () => {
-    dispatch({ type: ACTIONS.DISPLAY_FAVOURITE_PHOTOS });
-  }
+  // const handleShowFavourites = () => {
+  //   dispatch({ type: ACTIONS.DISPLAY_FAVOURITE_PHOTOS });
+  // }
 
   // SELECT DATA FOR A SPECIFIC PHOTO 
   const selectPhoto = (photo) => {
